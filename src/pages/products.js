f1016ba15595b737e0832697bcffcd6e740580c4
Rakshit <rakshit@mobifyi.com>
2021-09-07 12:57:23 +0530

    Added new UI for case study

---
+++ src/pages/products.js
@@ -0,0 +1,18 @@
+// // global imports
+// import "../toggleSidebar.js";
+// // import "../cart/toggleCart.js";
+// // import "../cart/setupCart.js";
+
+// // specific imports
+import { store } from "../store.js";
+import display from "../displayProducts.js";
+// import { getElement } from "../utils.js";
+
+// const loading = getElement(".page-loading");
+
+display(store, getElement(".products-container"));
+
+
+// setupCompanies(store);
+// setupPrice(store);
+// loading.style.display = "none";
